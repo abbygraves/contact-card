27dbb7e556739766b20943c79cdb83d1688c90cc
abbygraves <abbygraves14@gmail.com>
2022-09-09 13:05:45 -0500

    created, configured, and connected an IndexedDB database

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,12 +2,16 @@
 import "./form";
 import "./submit";
 
+// IMPORT DATABASE
+import { initdb } from './database';
+
 // IMPORT IMAGES
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
