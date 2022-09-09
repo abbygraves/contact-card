81e255c178c5b6c447f73b8e274d50804d43f8d7
abbygraves <abbygraves14@gmail.com>
2022-09-09 17:14:24 -0500

    rendering cards and using the form element

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,26 +1,65 @@
 // IMPORT MODULES
 import "./form";
-import "./submit";
 
 // IMPORT DATABASE
-import { initdb, getDb, postDb } from './database';
+import { initdb, getDb, postDb } from "./database";
 
 // IMPORT IMAGES
-import Logo from '../images/logo.png';
-import Bear from '../images/bear.png';
-import Dog from '../images/dog.png';
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
 
-window.addEventListener('load', function () {
+// IMPORT FETCH CARDS FUNCTION
+import { fetchCards } from "./cards";
+
+// IMPORT FORM FUNTIONS
+import { toggleForm, clearForm} from './form';
+
+// ON LOAD EVENT LISTENER
+window.addEventListener("load", function () {
   initdb();
-  // getDb();
-  // postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
-  // getDb();
-  document.getElementById('logo').src = Logo;
-  document.getElementById('bearThumbnail').src = Bear;
-  document.getElementById('dogThumbnail').src = Dog;
+  fetchCards();
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
 });
 
-// IMPORT CSS FILES & BOOTSTRAP 
+// FORM FUNCTIONALITY
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+});
+
+// IMPORT CSS FILES & BOOTSTRAP
 import "../css/index.css";
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
